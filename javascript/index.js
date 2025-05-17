694e70f2135061f7e87ca04ce22d16e856a5f00c
rupayan <rupbera922@gmail.com>
2025-05-17 11:16:51 +0530

    Pwc landing page first step done

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+let l = "Love Babbar";
+if (l=="Love Babbar") {
+   let a=1;
+   while (a<=4){
+    console.log("Thank You Love Babbar Bhaiya");
+    if (a==3){
+       
+        continue;
+    }
+    a++;
+   }
+}
